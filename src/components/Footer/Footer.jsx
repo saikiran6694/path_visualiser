2e7e658208267dddb159c55edc6be6220a6818cf
saikiranbembalge <saikiranbembalge123@gmail.com>
2023-08-17 21:33:25 +0530

    first

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-center">
+      <a
+        href="https://www.linkedin.com/in/sethuram-s-v-171719194/"
+        className="fa fa-linkedin"
+      ></a>
+      <a href="https://github.com/Sethuram52001" className="fa fa-github"></a>
+      <a href="https://sethuram52001.medium.com/" className="fa fa-medium"></a>
+    </div>
+  );
+};
+
+export default Footer;
