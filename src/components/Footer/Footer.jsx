68b3a5258f1c74880c2c6f49c66ef41392a410cd
Saikiran <70024114+saikiran6694@users.noreply.github.com>
2023-09-02 20:19:30 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,11 +5,10 @@ const Footer = () => {
   return (
     <div className="bg-dark text-center">
       <a
-        href="https://www.linkedin.com/in/sethuram-s-v-171719194/"
+        href="https://www.linkedin.com/in/saikiran-a73429232/"
         className="fa fa-linkedin"
       ></a>
-      <a href="https://github.com/Sethuram52001" className="fa fa-github"></a>
-      <a href="https://sethuram52001.medium.com/" className="fa fa-medium"></a>
+      <a href="https://github.com/saikiran6694" className="fa fa-github"></a>
     </div>
   );
 };
